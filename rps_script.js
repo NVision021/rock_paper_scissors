0bab242feb30f1758b4ca75acec9329747b84aef
NVision021 <nversic2@gmail.com>
2024-08-18 16:35:38 +1000

    Add playRound, playGame and related functions
    Game now has logic to determine winner, communicate winner and keep track of score

--- rps_script.js
+++ rps_script.js
@@ -6,12 +6,12 @@ function getComputerChoice() {
   if (numberChoice <= 1/3) {
     computerChoice = "rock";
   } else if (numberChoice > 1/3 && numberChoice < 2/3) {
-    computerChoiceChoice = "paper";
+    computerChoice = "paper";
   } else {
     computerChoice = "scissors";
   }
 
-  return rpsChoice;
+  return computerChoice;
 }
 
 /*Function that gets the user input */
@@ -19,9 +19,57 @@ function getHumanChoice() {
   return prompt("Rock, Paper or Scissors?").toLowerCase();
 }
 
+/*Global variables that record the score*/
+let humanScore = 0;
+let computerScore = 0;
 
-/*?Global variables, ?Local variables that record the score*/
+/*Functions that provides text and increases score as appropriate*/
+function computerWins(computerChoice, humanChoice) {
+  let capitalisedComputer = computerChoice.slice(0,1).toUpperCase() + computerChoice.slice(1).toLowerCase();
+  console.log(`Computer picked ${computerChoice}. You lose! ${capitalisedComputer} beats ${humanChoice}!`);
+  computerScore += 1;
+}
+
+function humanWins(computerChoice, humanChoice) {
+  let capitalisedHuman = humanChoice.slice(0,1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+  console.log(`Computer picked ${computerChoice}. You win! ${capitalisedHuman} beats ${computerChoice}!`);
+  humanScore += 1;
+}
+
 
 /*Function that plays a single round */
+function playRound () {
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice();
+
+  if (computerChoice === humanChoice) {
+    console.log("It's a Tie! No one wins!")
+  } else if (computerChoice === "rock" && humanChoice === "scissors" || computerChoice === "scissors" && humanChoice === "paper" || computerChoice === "paper" && humanChoice === "rock") {
+    computerWins(computerChoice, humanChoice)
+  } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
+    humanWins(computerChoice, humanChoice)
+  }
+
+  console.log("Scores:")
+  console.log(`Computer: ${computerScore}`)
+  console.log(`Player: ${humanScore}`)
+}
 
 /*Logic to play entire game */
+function playGame() {
+  playRound()
+  playRound()
+  playRound()
+  playRound()
+  playRound()
+
+  if (humanScore === computerScore) {
+    console.log("After 5 rounds, it's a Tie!")
+  } else if (humanScore > computerScore) {
+    console.log("After 5 rounds, you are the winner!")
+  } else {
+    console.log("After 5 rounds, the computer is the winner!")
+  }
+}
+
+playGame()
